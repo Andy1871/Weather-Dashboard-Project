f99b9e5f7b98b86d663d26415a11ecb8c803e58d
Andy1871 <andyherbert1875@gmail.com>
2025-12-17 15:21:07 +0000

    Update server.ts to stop login issues

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,25 +1,21 @@
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// To use inside Server Components, Server Actions, and Route Handlers 
 export function supabaseServer() {
+  const cookieStore = cookies();
+
   return createServerClient(url, anon, {
-    
     cookies: {
-      async get(name: string) {
-        const store = await cookies();
-        return store.get(name)?.value;
+      getAll() {
+        return cookieStore.getAll();
       },
-      async set(name: string, value: string, options?: CookieOptions) {
-        const store = await cookies();
-        store.set({ name, value, ...options });
-      },
-      async remove(name: string, options?: CookieOptions) {
-        const store = await cookies();
-        store.set({ name, value: "", ...options });
+      setAll(cookiesToSet) {
+        cookiesToSet.forEach(({ name, value, options }) => {
+          cookieStore.set(name, value, options);
+        });
       },
     },
   });
